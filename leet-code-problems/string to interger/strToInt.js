537572ce73f6268c013d76420d8b74ebe09a64b8
ronald <okpararonald0@gmail.com>
2025-11-01 15:09:52 -0700

    couldnt solve medium leet-code problem

---
+++ leet-code-problems/string to interger/strToInt.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    
+    // convert the string to an array of characters
+    let convertStr = s.split('');
+    // if()
+    // switch(convertStr){
+    //     case startsWith():
+    // }
+  
+    return convertStr
+};
+
+let result = myAtoi("337c0d3")
+console.log( result)
